7aaeb8df7dfabb011c3e7e33a74323bc2e78a7dc
lina <36717206+lina-bh@users.noreply.github.com>
2024-11-06 03:42:41 +0000

    stuff

--- flake.nix
+++ flake.nix
@@ -13,20 +13,43 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          allowUnfree = true;
+        };
       in
-      rec {
+      {
         lib = {
           gfxwrap = import ./lib/gfxwrap.nix { inherit pkgs; };
         };
 
         packages = {
           mpv-nonixos = pkgs.buildEnv {
-            inherit (pkgs.mpv) version;
             name = "mpv-nonixos";
             paths = [
               pkgs.mpv
-              (lib.gfxwrap "mpv" "${pkgs.mpv}/bin/mpv")
+              (self.lib.${system}.gfxwrap "mpv" "${pkgs.mpv}/bin/mpv")
+            ];
+          };
+
+          jellyfin-mpv-shim = pkgs.jellyfin-mpv-shim.override {
+            mpv = self.packages.${system}.mpv-nonixos;
+          };
+
+          discord-openasar = pkgs.discord.override {
+            withOpenASAR = true;
+          };
+
+          tools = pkgs.buildEnv {
+            name = "tools";
+            paths = with pkgs; [
+              fd
+              biome
+              nix-direnv
+              nixfmt-rfc-style
+              ruff
+              yq
+              httpie
             ];
           };
         };
